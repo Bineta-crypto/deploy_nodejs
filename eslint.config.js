4a2edb7074443bf6967078ae0a3caf459704b4d8
Bineta-crypto <82139363+Bineta-crypto@users.noreply.github.com>
2024-12-01 14:38:40 +0100

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  extends: ['eslint:recommended'],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    // Ajoutez vos règles personnalisées ici si nécessaire
+  },
+};
